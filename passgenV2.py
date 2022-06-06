e2bfdebc50f761c7c9d7be199e781039b12a4d53
Marchkkkk <104899636+Marchkkkk@users.noreply.github.com>
2022-06-06 15:15:04 +0300

    Add files via upload

---
+++ passgenV2.py
@@ -0,0 +1,21 @@
+import random
+
+pas = ''
+list = ('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ') ##list of symbols that will be used
+
+passAmount = int (input('Ammount of passwords: '))
+symbolsamount = int (input('Ammount of symbols: '))
+f = open('resultpass.txt', 'w+')
+
+
+for x in range(1, passAmount+1):
+
+  for x in range(1, symbolsamount+1):
+    pas = pas + random.choice(list)
+
+  f.write(pas + '\n')
+  print(pas)
+  pas =''
+f.close()
+
+
